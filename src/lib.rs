584a6574910cdc5b2a61b8f0b161886d0912cbd9
manishdoley23 <manishdoley23@gmail.com>
2024-04-11 13:14:40 +0530

    redo to operator overload

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,30 @@
 pub mod graphic_vec {
+    use std::ops::{Add, Sub};
+
     pub struct Vec3 {
         color: Vec<f64>,
+    }
+
+    impl Add for Vec3 {
+        type Output = Self;
+        fn add(self, other: Self) -> Self {
+            Vec3 {
+                color: vec![
+                    self.get_val(0) + other.get_val(0),
+                    self.get_val(1) + other.get_val(1),
+                    self.get_val(2) + other.get_val(2),
+                ],
+            }
+        }
+    }
+
+    impl Sub for Vec3 {
+        type Output = Self;
+        fn sub(self) -> Self {
+            Vec3 {
+                color: vec![-self.get_val(0), -self.get_val(1), -self.get_val(2)],
+            }
+        }
     }
 
     impl Vec3 {
@@ -45,20 +69,44 @@ pub mod graphic_vec {
             self.color.get(index).unwrap_or(&0.0)
         }
 
-        fn operation_on_value(&self, index: usize, operation: char, value: f64) -> f64 {
-            match operation {
-                '+' => self.color.get(index).cloned().unwrap_or(0.0) + value,
-                '*' => self.color.get(index).cloned().unwrap_or(0.0) * value,
-                _ => 0.0,
-            }
-        }
+        // fn operation_add_helper(&self, index: usize, value: f64) -> f64 {
+        //     match operation {
+        //         '+' => self.color.get(index).cloned().unwrap_or(0.0) + value,
+        //         '*' => self.color.get(index).cloned().unwrap_or(0.0) * value,
+        //         _ => 0.0,
+        //     }
+        // }
         pub fn operator_add(&mut self, vec3: &Self) -> &Self {
             self.color = vec![
-                self.operation_on_value(0, '+', vec3.get_x()),
-                self.operation_on_value(1, '+', vec3.get_y()),
-                self.operation_on_value(2, '+', vec3.get_z()),
+                self.get_val(0) + vec3.get_x(),
+                self.get_val(1) + vec3.get_y(),
+                self.get_val(2) + vec3.get_z(),
             ];
             self
         }
+
+        pub fn operator_multiply(&mut self, val: f64) -> &Self {
+            for ele in self.color.iter_mut() {
+                *ele *= val;
+            }
+            self
+        }
+
+        pub fn operator_divide(&mut self, val: f64) -> &Self {
+            for ele in self.color.iter_mut() {
+                *ele *= 1.0 / val;
+            }
+            self
+        }
+
+        pub fn length(&self) -> f64 {
+            let mut sum = 0.0;
+            for ele in self.color.iter() {
+                sum += ele * ele;
+            }
+            sum.sqrt()
+        }
     }
+
+    type Point3 = Vec3;
 }
